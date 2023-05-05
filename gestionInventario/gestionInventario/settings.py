be9fe4484a8c5fa7282a993ea081a6291db5632d
Juan Laguna <juansebt.0610@gmail.com>
2023-05-05 09:17:02 -0500

    third commit

--- gestionInventario/gestionInventario/settings.py
+++ gestionInventario/gestionInventario/settings.py
@@ -75,14 +75,12 @@ WSGI_APPLICATION = 'gestionInventario.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""
 DATABASES_SQLLITE = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
 DATABASES = {
     'default': {
