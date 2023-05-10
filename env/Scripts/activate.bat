48ca83ef7765809b67c29369fd20ca248f9f301e
Juan Laguna <juansebt.0610@gmail.com>
2023-05-10 06:32:52 -0500

    nineth commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Juan Sebastian Laguna Yara\Python\Django\Proyecto inventario\Proyecto_inventario\env
+set VIRTUAL_ENV=D:\Juan Sebastian Laguna Yara\Python\Django\Proyecto inventario\Proyecto_inventario\env
 
 if not defined PROMPT set PROMPT=$P$G
 
